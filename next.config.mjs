4f387f3a7d747285bbe6e75970886c6ea07e65d1
Aamir Azad <aamirmazad@gmail.com>
2025-12-06 12:47:08 -0500

    feat: remove video player as well It was causing trouble

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import createMDX from "@next/mdx";
-
 // @ts-check
 
 /**
@@ -183,10 +181,5 @@ const nextConfig = {
 	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-const withMDX = createMDX({
-	// Add markdown plugins here, as desired
-	extension: /\.(md|mdx)$/,
-});
-
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default nextConfig;
