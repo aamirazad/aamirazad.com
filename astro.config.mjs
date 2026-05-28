0dcd5d02a5790ab777e5ff80b824937a253a1336
Aamir Azad <aamirmazad@gmail.com>
2026-05-28 10:50:53 -0400

    feat: update projects and homelab and add redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,56 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://aamirazad.com",
-  compressHTML: true
+	site: "https://aamirazad.com",
+	compressHTML: true,
+	redirects: {
+		"/cs50-cert":
+			"https://courses.edx.org/certificates/0a9007180d26449b8593df3ef23a1400",
+		"/github": "https://github.com/aamirazad/",
+		"/resume": "https://files.aamirazad.com/resume.pdf",
+		"/signal":
+			"https://signal.me/#eu/gYSBVtWgFaykPtlA00TXJrO91sPmPjq_Si4wE8oYpd1E-llhxxVBtITfDB17DtDj",
+		"/telegram": "https://t.me/aamirazad01",
+		"/pgp": "/.well-known/openpgpkey",
+		"/bycda": "https://hasd.dino.icu/category/5/bycda",
+		"/.well-known/openpgpkey": "/api/.well-known/openpgpkey",
+		"/.well-known/openid-configuration":
+			"/api/.well-known/openid-configuration",
+		"/outstanding-academic":
+			"https://papers.aamirazad.com/share/wyABSOjNExeQi4nBVfS10rMH4LBkvc8ppvxZdoroEfNY7V9ECF",
+		"/principals-award":
+			"https://papers.aamirazad.com/share/FFrlAJyx3DgTU7N3eFqW9mGKY1NukmXZNmQv4ztosV4Su7ZFhZ",
+		"/perfect-attendance-2023":
+			"https://papers.aamirazad.com/share/PXPJTBMvauXaHVqwyqHtHhNF18n4ayakr8Uu0dUsOVmaArbyOd",
+		"/presidents-award":
+			"https://papers.aamirazad.com/share/hAZ5KImbXUlB3Ul03zV5YtSxY4Oo8XjGQNJk0b7SXvqLyt7yFj",
+		"/american-citizenship-award":
+			"https://papers.aamirazad.com/share/hWSsF4LSg5DJ4DaWQCoTi2a49X2pEodAeJSPqhHB5ER2NOA9qr",
+		"/profile-picture":
+			"https://azadphotos.com/api/assets/83900471-95e1-4f33-a032-e41531e7455f/thumbnail?size=preview&key=6Cy6QJLTizf6dILKFfjfm9SJcfZjmR-Dzbk1yIHZFOQwSkAOYrk4QY7LKa0wxOSazU8&c=Y2WyiAHtxlI37LaS42gHmgXhpnI%3D",
+		"/fbla-game":
+			"https://connect.fbla.org/headquarters/files/High%20School%20Competitive%20Events%20Resources/Individual%20Guidelines/Presentation%20Events/Computer-Game-Simulation-Programming.pdf",
+		"/aseprite": "https://files.aamirazad.com/inbox/aseprite-v1.3.14-beta1.zip",
+		"/pronounce-godot":
+			"https://upload.wikimedia.org/wikipedia/commons/transcoded/e/ef/En-us-Godot.oga/En-us-Godot.oga.mp3",
+		"/zulip": "https://hasd.zulipchat.com/join/6rwf4ado2v2erh3rowedivlr/",
+		"/fbla-zulip": "https://hasd.zulipchat.com/join/ki2oh6q5q3mnr3td6r3fxplg/",
+		"/history-zulip":
+			"https://hasd.zulipchat.com/join/gk442fdoiubirhzpdytxvq5w/",
+		"/history-officer-zulip":
+			"https://hasd.zulipchat.com/join/h3fovacw2avg7c52vrftxt4k/",
+		"/chem-textbook": "https://cdn.aamirazad.com/Zumdahl%20Textbook.pdf",
+		"/fbla-2024-gc-archive":
+			"https://files.aamirazad.com/inbox/FBLA%202024-2025%20GC%20Stream.html",
+		"/turkey-2025": "/watch/AY4aAI7zzcaOE6Kd3202XudT7k4jIuglQSIBj3UoGu02U",
+		"/backpack":
+			"https://www.change.org/p/end-the-backpack-ban-at-hasd-high-school?recruited_by_id=cee357a0-8114-11f0-9397-51d3dd62874f&utm_source=share_petition&utm_campaign=psf_combo_share_initial&utm_medium=copylink",
+		"/.well-known/matrix/server": "/api/.well-known/matrix/server",
+		"/ssh": "/api/ssh",
+		"/api/script.js": "https://analytics.aamirazad.com/api/script.js",
+		"/api/track": "https://analytics.aamirazad.com/api/track",
+		"/codeberg": "https://codeberg.org/aamir",
+		"/bluesky": "https://bsky.app/profile/aamirazad.com",
+		"/mastodon": "https://mastodon.social/@aamira",
+	},
 });
--- src/data/links.ts
+++ src/data/links.ts
@@ -1,177 +1,175 @@
 export const contactLinks = [
-  {
-    href: "/github",
-    label: "github"
-  },
-  {
-    href: "/resume",
-    label: "resume"
-  },
-  {
-    href: "/pgp",
-    label: "pgp"
-  },
-  {
-    href: "mailto:aamirmazad@gmail.com",
-    label: "email"
-  },
-  {
-    href: "/signal",
-    label: "signal"
-  },
-  {
-    href: "/telegram",
-    label: "telegram"
-  },
-  {
-    href: "https://bsky.app/profile/aamirazad.com",
-    label: "bluesky"
-  },
-  {
-    href: "https://mastodon.social/@aamira",
-    label: "mastodon"
-  }
+	{
+		href: "/github",
+		label: "github",
+	},
+	{
+		href: "/codeberg",
+		label: "codeberg",
+	},
+	{
+		href: "/resume",
+		label: "resume",
+	},
+	{
+		href: "/pgp",
+		label: "pgp",
+	},
+	{
+		href: "mailto:aamirmazad@gmail.com",
+		label: "email",
+	},
+	{
+		href: "/signal",
+		label: "signal",
+	},
+	{
+		href: "/telegram",
+		label: "telegram",
+	},
+	{
+		href: "bluesky",
+		label: "bluesky",
+	},
+	{
+		href: "mastodon",
+		label: "mastodon",
+	},
 ];
 
 export const projects = [
-  {
-    href: "https://sim.aamirazad.com",
-    name: "WWII Simulation",
-    description:
-      "Learn history by playing it. Coordinate and strategize as nations in the Second World War. Wage war, forge alliances, and have fun.",
-    wip: true,
-    badge: "Featured"
-  },
-  {
-    href: "https://track.aamirazad.com",
-    name: "TrackShelf",
-    description:
-      "TrackShelf is an app to manage all the books, movies, and tv shows you watch so that you never forget what about these experiences was special to you.",
-    github: "aamirazad/track"
-  },
-  {
-    href: "https://fbla.notion.site/",
-    name: "FBLA Site",
-    description:
-      "A notion backed website to serve as a resource for finding information about FBLA specific activities. Namely, a list of events and their information is hosted on this site."
-  },
-  {
-    href: "https://historyclub.aamirazad.com/",
-    name: "History Club Site",
-    description:
-      "A content heavy application built with Astro to host information about the HASD History Club's WW2 Simulation. This game, with very complex rules, will be available for others to reference on this pretty website.",
-    github: "aamirazad/history-club"
-  },
-  {
-    href: "https://jobs.aamirazad.com/",
-    name: "Job Journey",
-    description:
-      "A full stack nextjs app in which students can easily search job postings and employers can easily submit them with secure authentication and ease of use. Project was awarded 3rd place in the Pennsylvania FBLA Competition.",
-    badge: "Featured",
-    github: "aamirazad/job-journey"
-  },
-  {
-    href: "https://tigertutoringtool.aamirazad.com/",
-    name: "Tigertutoringtool",
-    description:
-      "Written articles to help students in my class with their classes. Features charts and diagrams as well as linking between articles and a graph view.",
-    github: "aamirazad/tigertutoringtool"
-  },
-  {
-    href: "https://homelab-connector.aamirazad.com",
-    name: "Homelab Connector",
-    description:
-      "A lightning fast full stack nextjs app connecting many self hosted services including paperless-ngx, immich, and whisper with authentication and support for many different use cases.",
-    github: "aamirazad/homelab-connector"
-  },
-  {
-    href: "https://school.aamirazad.com/threegurlsrunnin",
-    name: "Donation Tracker",
-    description: "A simple display of how much money has been raised",
-    github: "aamirazad/school/tree/main/src/app/threegurlsrunnin"
-  },
-  {
-    href: "https://school.aamirazad.com/%CE%94",
-    name: "Δ Chem",
-    description:
-      "Delta math inspired chemistry quiz site. Walks the user though solving a problem to teach a concept efficiently.",
-    github: "aamirazad/school/tree/main/src/app/%25CE%2594"
-  },
-  {
-    href: "https://school.aamirazad.com/renaissance-image-collection",
-    name: "Renaissance Image Collection",
-    description: "An interactive slideshow of images and information",
-    github: "aamirazad/school/tree/main/src/app/renaissance-image-collection"
-  },
-  {
-    href: "https://school.aamirazad.com/enlightenment-french-revolution-timeline",
-    name: "Enlightenment/French Revolution Timeline",
-    description: "An interactive timeline made with Timeline JS",
-    github: "aamirazad/school/tree/main/src/app/enlightenment-french-revolution-timeline"
-  },
-  {
-    href: "https://school.aamirazad.com/open-source",
-    name: "Open Source Writeup",
-    description: "A simple html static site about open source",
-    github: "aamirazad/school/tree/main/src/app/open-source"
-  }
+	{
+		href: "https://terranaut.aamirazad.com",
+		name: "Terranaut",
+		description:
+			"A open source social fitness app designed to motivate users to get outside and accomplish their fitness goals.",
+		github: "aamirazad/terranaut",
+		badge: "Featured",
+		wip: true,
+	},
+	{
+		href: "https://sim.aamirazad.com",
+		name: "WWII Simulation",
+		description:
+			"Learn history by playing it. Coordinate and strategize as nations in the Second World War. Wage war, forge alliances, and have fun.",
+		github: "aamirazad/wwii-simulation",
+		badge: "Featured",
+		wip: false,
+	},
+	{
+		name: "TrackShelf",
+		description:
+			"TrackShelf is an app to manage all the books, movies, and tv shows you watch so that you never forget what about these experiences was special to you.",
+		github: "aamirazad/track",
+	},
+	{
+		href: "https://fbla.notion.site/",
+		name: "FBLA Site",
+		description:
+			"A notion backed website to serve as a resource for finding information about FBLA specific activities. Namely, a list of events and their information is hosted on this site.",
+	},
+	{
+		href: "https://historyclub.aamirazad.com/",
+		name: "History Club Site",
+		description:
+			"A content heavy application built with Astro to host information about the HASD History Club's WW2 Simulation. This game, with very complex rules, will be available for others to reference on this pretty website.",
+		github: "aamirazad/history-club",
+	},
+	{
+		href: "https://jobs.aamirazad.com/",
+		name: "Job Journey",
+		description:
+			"A full stack nextjs app in which students can easily search job postings and employers can easily submit them with secure authentication and ease of use. Project was awarded 3rd place in the Pennsylvania FBLA Competition.",
+		badge: "Featured",
+		github: "aamirazad/job-journey",
+	},
+	{
+		href: "https://tigertutoringtool.aamirazad.com/",
+		name: "Tigertutoringtool",
+		description:
+			"Written articles to help students in my class with their classes. Features charts and diagrams as well as linking between articles and a graph view.",
+		github: "aamirazad/tigertutoringtool",
+	},
+	{
+		href: "https://homelab-connector.aamirazad.com",
+		name: "Homelab Connector",
+		description:
+			"A lightning fast full stack nextjs app connecting many self hosted services including paperless-ngx, immich, and whisper with authentication and support for many different use cases.",
+		github: "aamirazad/homelab-connector",
+	},
+	{
+		href: "https://school.aamirazad.com/threegurlsrunnin",
+		name: "Donation Tracker",
+		description: "A simple display of how much money has been raised",
+		github: "aamirazad/school/tree/main/src/app/threegurlsrunnin",
+	},
+	{
+		href: "https://school.aamirazad.com/%CE%94",
+		name: "Δ Chem",
+		description:
+			"Delta math inspired chemistry quiz site. Walks the user though solving a problem to teach a concept efficiently.",
+		github: "aamirazad/school/tree/main/src/app/%25CE%2594",
+	},
+	{
+		href: "https://school.aamirazad.com/renaissance-image-collection",
+		name: "Renaissance Image Collection",
+		description: "An interactive slideshow of images and information",
+		github: "aamirazad/school/tree/main/src/app/renaissance-image-collection",
+	},
+	{
+		href: "https://school.aamirazad.com/enlightenment-french-revolution-timeline",
+		name: "Enlightenment/French Revolution Timeline",
+		description: "An interactive timeline made with Timeline JS",
+		github:
+			"aamirazad/school/tree/main/src/app/enlightenment-french-revolution-timeline",
+	},
+	{
+		href: "https://school.aamirazad.com/open-source",
+		name: "Open Source Writeup",
+		description: "A simple html static site about open source",
+		github: "aamirazad/school/tree/main/src/app/open-source",
+	},
 ];
 
 export const homelab = [
-  {
-    name: "Paperless-ngx",
-    href: "https://papers.aamirazad.com/"
-  },
-  {
-    name: "Rybbit",
-    href: "https://analytics.aamirazad.com/"
-  },
-  {
-    name: "Immich",
-    href: "https://photos.aamirazad.com/"
-  },
-  {
-    name: "Forgejo",
-    href: "https://code.aamirazad.com/"
-  },
-  {
-    name: "Jellyfin",
-    href: "https://jellyfin.aamirazad.com/"
-  },
-  {
-    name: "Rallly",
-    href: "https://meetup.aamirazad.com/"
-  },
-  {
-    name: "Pocket ID",
-    href: "https://auth.aamirazad.com/"
-  },
-  {
-    name: "Mealie",
-    href: "https://recipies.aamirazad.com/"
-  },
-  {
-    name: "LanguageTool",
-    href: "https://languagetool.aamirazad.com"
-  },
-  {
-    name: "Linkding",
-    href: "https://bookmarks.aamirazad.com/"
-  },
-  {
-    name: "Copyparty",
-    href: "https://files.aamirazad.com/"
-  },
-  {
-    name: "Writefreely",
-    href: "https://docs.aamirazad.com/"
-  },
-  {
-    name: "Actual",
-    href: "https://finance.aamirazad.com/"
-  },
-  {
-    name: "Code Server",
-    href: "https://dev.aamirazad.com/"
-  }
+	{
+		name: "Immich",
+		href: "https://photos.aamirazad.com/",
+	},
+	{
+		name: "Paperless-ngx",
+		href: "https://papers.aamirazad.com/",
+	},
+	{
+		name: "Forgejo",
+	},
+	{
+		name: "Jellyfin",
+	},
+	{
+		name: "Rybbit",
+		href: "https://analytics.aamirazad.com/",
+	},
+	{
+		name: "Pocket ID",
+		href: "https://auth.aamirazad.com/",
+	},
+	{
+		name: "Copyparty",
+		href: "https://files.aamirazad.com/",
+	},
+	{
+		name: "Mealie",
+	},
+	{
+		name: "LanguageTool",
+	},
+	{
+		name: "Linkding",
+		href: "https://bookmarks.aamirazad.com/",
+	},
+	{
+		name: "Actual",
+		href: "https://finance.aamirazad.com/",
+	},
 ];
